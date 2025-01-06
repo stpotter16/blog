1bee927f04d5758d0058fa25528da9c2d1cb6dde
stpotter16 <stpotter1642@gmail.com>
2025-01-06 05:48:30 -0500

    Add rss feed

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,6 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    site: "https://gentle-travesseiro-a592aa.netlify.app/"
+});
